27088f4027d65f62a529a458023d96d7b9eaa88a
Dreadnogh <56774158+Dreadnogh@users.noreply.github.com>
2020-12-04 13:41:47 +0100

    Add productcontain and employeecontainer
    Signed-off-by: Dreadnogh <56774158+Dreadnogh@users.noreply.github.com>

--- src/model/ProductContainer.java
+++ src/model/ProductContainer.java
@@ -1,5 +1,30 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductContainer {
+	private ProductContainer instance; 
+	private Map<String , Product> products;
+	
+	private ProductContainer() {
+		products = new HashMap<>();
+	}
+	
+	public ProductContainer getInstance() {
+		if(instance == null){
+			instance = new ProductContainer();
+		}
+		return instance;
+	}
+	
+	public Product getProduct(String barcode) {
+		return products.get(barcode);
+	}
+	
+	public void addProduct(String barcode, Product product) {
+		products.put(barcode, product);
+	}
+	
 
 }
