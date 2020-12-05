3823ace2a90b2b1cfc144b49337c668df7e19a96
Michael <michael-graversen@hotmail.com>
2020-12-05 09:22:51 +0100

    New prompt and date test

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,17 @@
 package main;
 
-import tui.Menu;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import textinput.TextInput;
 
 public class Main {
 	public static void main(String[] args) {
-		new Menu("Main menu").start();
+//		new Menu("Main menu").start();
+
+		Date date = new TextInput().promptDate("Whats the date?", "dd/MM/yyyy");
+		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+		System.out.println(df.format(date));
 	}
 }
