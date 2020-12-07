4a63c86546de0b7d3e9e4b0714480ae135e6dde8
Dreadnogh <56774158+Dreadnogh@users.noreply.github.com>
2020-12-07 13:59:17 +0100

    Update CreateOrderOption.java

--- src/tui/option/CreateOrderOption.java
+++ src/tui/option/CreateOrderOption.java
@@ -14,12 +14,21 @@ public class CreateOrderOption extends Option {
 	//Mock data for create order.
 	public void start() {
 		OrderController orderCon = new OrderController();
+		orderCon.createOrder();
 		System.out.println("****** " + getDescription() + "******");
 		//TODO Implement function.
 		System.out.println("Scan products now - ");
 		
-		TextInput textinput = new TextInput();
-		String barcode = textinput.promptString("Enter Barcode: ");
+		boolean done = false;
+		while(!done) {
+			TextInput textinput = new TextInput();
+			String barcode = textinput.promptString("Enter Barcode [press 0 to stop]: ");
+			orderCon.getProduct(barcode);
+			
+			if(barcode.equals(0)) {
+				done = true;
+			}
+		}
 	}
 
 
