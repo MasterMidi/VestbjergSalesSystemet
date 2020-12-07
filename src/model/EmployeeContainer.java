fe24c023b8c20d93162b0c1184551d0569fc9660
bukz <julius@juto.dk>
2020-12-07 09:53:36 +0100

    Employee controller & Employee container done

--- src/model/EmployeeContainer.java
+++ src/model/EmployeeContainer.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class EmployeeContainer {
-	private EmployeeContainer instance;
+	private static EmployeeContainer instance;
 	private Map<Integer, Employee> employees;
 
 	private EmployeeContainer() {
 		employees = new HashMap<>();
 	}
 
-	public EmployeeContainer getInstance() {
+	public static EmployeeContainer getInstance() {
 		if (instance == null) {
 			instance = new EmployeeContainer();
 		}
