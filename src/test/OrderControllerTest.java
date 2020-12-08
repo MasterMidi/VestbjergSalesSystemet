11aeb5217f2b0e3c6d14434ef149312ea72941f5
bukz <julius@juto.dk>
2020-12-08 10:51:49 +0100

    Wrote a bit on Order tests

--- src/test/OrderControllerTest.java
+++ src/test/OrderControllerTest.java
@@ -1,11 +1,17 @@
 package test;
 
+import static org.junit.Assert.assertSame;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import controller.EmployeeController;
 import controller.OrderController;
+import controller.ProductController;
 import model.OrderContainer;
+import model.PaymentMethod;
+import model.PersonRole;
 
 class OrderControllerTest {
 
@@ -18,6 +24,17 @@ class OrderControllerTest {
 		container = OrderContainer.getInstance();
 		container.clear();
 		
+		//Creates a temporary employee to login
+		EmployeeController employeeController = new EmployeeController();
+		employeeController.createEmployee("123", "mail", "name", 1234, PersonRole.manager);
+		employeeController.login(1234);
+		
+		
+		ProductController productController = new ProductController();
+		productController.createSellableProduct("Magnuses gamle sko", "1234", "de lugter meget", 10d,2);
+		productController.createSellableProduct("Kaffe", "12345", "Nem og hurtig energi!", 15d,20);
+		productController.createSellableProduct("Energidrik", "12346", "Nem og hurtig energi!", 15d,20);
+		
 	}
 
 	@AfterEach
@@ -25,8 +42,30 @@ class OrderControllerTest {
 	}
 
 	@Test
-	void test() {
+	void testProcessSale() {
 		
+		controller.createOrder();
+		//controller.scanProduct("1234");
+		//controller.scanProduct("12345");
+		controller.finishSale(PaymentMethod.creditcard);
+		
+		controller.createOrder();
+		//controller.scanProduct("12346");
+		
+		controller.finishSale(PaymentMethod.cash);
+		assertSame(container.getOrderCount(),2);
+	}
+	
+	@Test
+	void testEditPrice() {
+		controller.createOrder();
+		//controller.scanProduct("1234");
+		//controller.scanProduct("12345");
+		controller.editProductPrice(0, 100d);
+		
+		double orderPrice = controller.getCurrentOrder().getOrderLineList().get(0).getPrice();
+		
+		assertSame(orderPrice,100d);
 	}
 
 }
