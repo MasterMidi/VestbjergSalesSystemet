c7cc2ff8ea9d3bc024dd2b444a588556f5f146ae
Nicolai Harms <nicolai.harms@gmail.com>
2021-01-04 10:49:00 +0100

    Update ProductContainer.java

--- src/model/product/ProductContainer.java
+++ src/model/product/ProductContainer.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import model.people.Person;
+
 public class ProductContainer {
 	private static ProductContainer instance;
 	private Map<String, Product> products;
@@ -27,19 +29,32 @@ public class ProductContainer {
 	public void addProduct(Product product) {
 		products.put(product.getBarcode(), product);
 	}
-	
-	public int getProductCount()
-	{
+
+	public int getProductCount() {
 		return products.size();
 	}
-	
-	public void clear()
-	{
+
+	public void clear() {
 		products.clear();
 	}
 
 	public List<Product> getProducts(String barcode) {
-		return new ArrayList<>(products.values());
-	}
+		List<Product> productList = new ArrayList<>();
 
+		boolean isName = barcode.trim().matches("[^\\d]+");
+
+		for (Product product : products.values()) {
+			if (isName) {
+				if (product.getName().contains(barcode)) {
+					productList.add(product);
+				}
+
+			} else {
+				if (product.getBarcode().contains(barcode)) {
+					productList.add(product);
+				}
+			}
+		}
+		return productList;
+	}
 }
